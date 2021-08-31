d98a681084f3cdcd4a4be604d4a1724f8519da77
Vic Shóstak <vic@shostak.dev>
2021-08-31 08:28:11 +0300

    Fix comments

--- sender.go
+++ sender.go
@@ -24,30 +24,6 @@ func NewEmailSender(login, password, server string, port int) *Sender {
 
 // SendHTMLEmail func for send email with given HTML template and data.
 // If template is empty string, it will throw error.
-//
-// Example:
-//
-// // Create a new struct for the email data.
-// type HTMLEmailData struct {
-//     Name    string
-//     Website	string
-// }
-//
-// // Create a new SMTP sender instance with your auth params.
-// sender := NewEmailSender("mail@test.com", "secret", "smtp.test.com", 25)
-//
-// // Send the email with HTML template.
-// if err := sender.SendHTMLEmail(
-//     "my/templates/welcome.html",
-//     []string{"mail@example.com"},
-//     "It's a test email!",
-//     &HTMLEmailData{
-//         Name:    "Vic",
-//         Website: "https://shostak.dev/",
-//     },
-// ); err != nil {
-//     return fmt.Errorf("Something went wrong: %v", err)
-// }
 func (s *Sender) SendHTMLEmail(templatePath string, dest []string, subject string, data interface{}) error {
 	if templatePath == "" {
 		return fmt.Errorf("Template not found in the given path!")
@@ -64,29 +40,6 @@ func (s *Sender) SendHTMLEmail(templatePath string, dest []string, subject strin
 }
 
 // SendPlainEmail func for send plain text email with data.
-//
-// Example:
-//
-// // Create a new struct for the email data.
-// type PlainEmailData struct {
-//     Company string
-//     Website string
-// }
-//
-// // Create a new SMTP sender instance with your auth params.
-// sender := NewEmailSender("mail@test.com", "secret", "smtp.test.com", 25)
-//
-// // Send the email with plain text.
-// if err := sender.SendPlainEmail(
-//     []string{"mail@example.com"},
-//     "It's a test email!",
-//     &PlainEmailData{
-//         Company: "True web artisans",
-//         Website: "https://1wa.co/",
-//     },
-// ); err != nil {
-//     return fmt.Errorf("Something went wrong: %v", err)
-// }
 func (s *Sender) SendPlainEmail(dest []string, subject, data string) error {
 	body := s.writeEmail(dest, "text/plain", subject, data)
 	if errSendEmail := s.sendEmail(dest, subject, body); errSendEmail != nil {
